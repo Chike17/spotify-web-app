07340105a289daa1a18e6b4514b32c2a1409c636
Chike <chike.onuorah@gmail.com>
2018-04-15 13:45:11 -0700

    extract cover photo address from css file into js

--- 1client/src/Container.js
+++ 1client/src/Container.js
@@ -2,6 +2,12 @@ import React from 'react';
 import styles from './styles.css';
 import Table from './Table.js';
 
+let divStyle = {
+  background: 'url("https://angstyteenwatchingtoomuchtv.files.wordpress.com/2015/07/tumblr_nlhsir3adc1sk2qobo1_12801.gif")',
+  "background-size": "cover"
+};
+
+
 let Container = () => {
   return (
    <div >
@@ -9,7 +15,7 @@ let Container = () => {
      <div className = {styles.screen}>
        <input type = "checkbox" value = "None" className = {styles.magicButton} name="check" />
        <div> Input Box </div>
-       <div className = {styles.coverImage}></div>
+       <div className = {styles.coverImage} style = {divStyle}></div>
        <div className = {styles.bodyPlayer}></div>
        <div className = {styles.testing}> Partial Result Data </div>
       <Table />
